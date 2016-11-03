6b7a6213e22624b2e4944c36fe8ad485f421865a
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-11-03 19:16:44 +0000

    Add -|- to PartialOrder

--- PartialOrder.hs
+++ PartialOrder.hs
@@ -9,13 +9,12 @@ class PartialOrder a where
     x -<- y = y ->- x
     (->-) :: a -> a -> Bool
     x ->- y = y -<- x
+    (-|-) :: a -> a -> Bool
+    x -|- y = not (x -<- y) && not (x ->- y)
 
 infix 4 -<-
 infix 4 ->-
-
--- instance Ord a => PartialOrder a where
---     (-<-) = (<=)
---     (->-) = (>=)
+infix 4 -|-
 
 instance Ord a => PartialOrder (Set.Set a) where
     (-<-) = Set.isSubsetOf
