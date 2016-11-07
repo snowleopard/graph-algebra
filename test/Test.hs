4231dd5ff5c0082a56842b7222c3b541a8655df4
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-11-07 00:54:52 +0000

    Refactor library tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,72 +1,60 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Data.Foldable
-import Test.QuickCheck
 
-import Basic
+import Basic hiding (test)
+import Demo
 import Graph
 import PartialOrder
 
-type DirectedGraph = Basic Int
--- Overlay properties:
-overlayIdentity :: DirectedGraph -> Bool
-overlayIdentity x = x + empty == x
-
-overlayCommutativity :: DirectedGraph -> DirectedGraph -> Bool
-overlayCommutativity x y = x + y == y + x
-
-overlayAssociativity :: DirectedGraph -> DirectedGraph -> DirectedGraph -> Bool
-overlayAssociativity x y z = x + (y + z) == (x + y) + z
-
-overlayIdempotence :: DirectedGraph -> Bool
-overlayIdempotence x = x + x == x
-
--- Connect properties:
-connectIdentity :: DirectedGraph -> Bool
-connectIdentity x = x * empty == x && empty * x == x
-
-connectAssociativity :: DirectedGraph -> DirectedGraph -> DirectedGraph -> Bool
-connectAssociativity x y z = x * (y * z) == (x * y) * z
-
--- Other properties:
-distributivity :: DirectedGraph -> DirectedGraph -> DirectedGraph -> Bool
-distributivity x y z = x * (y + z) == x * y + x * z && (x + y) * z == x * z + y * z
-
-decomposition :: DirectedGraph -> DirectedGraph -> DirectedGraph -> Bool
-decomposition x y z = x * y * z == x * y + x * z + y * z
-
-absorption :: DirectedGraph -> DirectedGraph -> Bool
-absorption x y = x + x * y == x * y && y + x * y == x * y
-
--- Test the partial order on graphs:
-partialOrder :: DirectedGraph -> DirectedGraph -> Bool
-partialOrder x y
-    | x -<- y && x ->- y = x == y
-    | x -<- y            = x + y == y
-    | x ->- y            = x + y == x
-    | x -|- y            = x /= y
-    | otherwise          = False
-
-lowerBound :: DirectedGraph -> Bool
-lowerBound x = empty -<- x
-
-upperBound :: DirectedGraph -> Bool
-upperBound x = x -<- vertices (toList x) * vertices (toList x)
-
-overlayConnectOrder :: DirectedGraph -> DirectedGraph -> Bool
-overlayConnectOrder x y = x + y -<- x * y
+type G = Basic Int
 
 main :: IO ()
 main = do
     putStrLn "============ Testing library ============"
-    quickCheck overlayIdentity
-    quickCheck overlayCommutativity
-    quickCheck overlayAssociativity
-    quickCheck overlayIdempotence
-    quickCheck connectIdentity
-    quickCheck connectAssociativity
-    quickCheck distributivity
-    quickCheck decomposition
-    quickCheck absorption
-    quickCheck partialOrder
-    quickCheck lowerBound
-    quickCheck upperBound
-    quickCheck overlayConnectOrder
+
+    test "Overlay identity" $ \(x :: G) ->
+        x + empty == x
+
+    test "Overlay commutativity" $ \(x :: G) y ->
+        x + y == y + x
+
+    test "Overlay associativity" $ \(x :: G) y z ->
+        x + (y + z) == (x + y) + z
+
+    test "Overlay idempotence" $ \(x :: G) ->
+        x + x == x
+
+    test "Connect identity" $ \(x :: G) ->
+        x * empty == x && empty * x == x
+
+    test "Overlay associativity" $ \(x :: G) y z ->
+        x * (y * z) == (x * y) * z
+
+    test "Distributivity" $ \(x :: G) y z ->
+        x * (y + z) == x * y + x * z && (x + y) * z == x * z + y * z
+
+    test "Decomposition" $ \(x :: G) y z ->
+        x * y * z == x * y + x * z + y * z
+
+    test "Absorption" $ \(x :: G) y ->
+        x + x * y == x * y && y + x * y == x * y
+
+    test "Lower bound" $ \(x :: G) ->
+        empty -<- x
+
+    test "Upper bound" $ \(x :: G) ->
+        x -<- vertices (toList x) * vertices (toList x)
+
+    test "Overlay-connect order" $ \(x :: G) y ->
+        x + y -<- x * y
+
+    test "Partial order" partialOrder
+  where
+    partialOrder :: G -> G -> Bool
+    partialOrder x y
+        | x -<- y && x ->- y = x == y
+        | x -<- y            = x + y == y
+        | x ->- y            = x + y == x
+        | x -|- y            = x /= y
+        | otherwise          = False
+
