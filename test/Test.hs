368051e1f7ed7ffa33f49242d04ef397deb05ec1
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-11-08 00:35:42 +0000

    Test PG

--- test/Test.hs
+++ test/Test.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 import Data.Foldable
+import Prelude hiding ((&&), (||), not)
 
-import Basic hiding (test)
+import Basic
 import Demo
 import Graph
 import PartialOrder
+import PG
 
 type G = Basic Int
+type P = PG Int Bool
 
 main :: IO ()
 main = do
@@ -38,6 +41,31 @@ main = do
 
     test "Absorption" $ \(x :: G) y ->
         x + x * y == x * y && y + x * y == x * y
+
+    test "True and false condition" $ \(x :: P) ->
+        True ? x == x && False ? x == empty
+
+    test "Conditional empty" $ \b ->
+        b ? empty == (empty :: P)
+
+    test "Conditional overlay" $ \(x :: P) y b ->
+        b ? (x + y) == b ? x + b ? y
+
+    test "Conditional connect" $ \(x :: P) y b ->
+        b ? (x * y) == b ? x * b ? y
+
+    test "AND condition" $ \(x :: P) b1 b2 ->
+        (b1 && b2) ? x == b1 ? b2 ? x
+
+    test "OR condition" $ \(x :: P) b1 b2 ->
+        (b1 || b2) ? x == b1 ? x + b2 ? x
+
+    test "Choice propagation" $ \(x :: P) y z b ->
+        b ? (x * y) + not b ? (x * z) == x * (b ? y + not b ? z) &&
+        b ? (x * z) + not b ? (y * z) == (b ? x + not b ? y) * z
+
+    test "Condition regularisation" $ \(x :: P) y b1 b2 ->
+        b1 ? x * b2 ? y == b1 ? x + b2 ? y + (b1 && b2) ? (x * y)
 
     test "Lower bound" $ \(x :: G) ->
         empty -<- x
